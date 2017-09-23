b522254306fe45b8a923ca2eeb6c795daf44260a
Grzegorz <grzegorz.petrykowski@gmail.com>
2017-09-23 17:13:49 +0100

    just before adding waypoints

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
 
 
 
