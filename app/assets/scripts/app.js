36bdf7f0fa2c6582c2b5c38bd149da69e44ee216
Grzegorz <grzegorz.petrykowski@gmail.com>
2017-09-24 18:56:24 +0100

    completed the reveal on scroll behaviour

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
-
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '60%');
+//^we can add as many reveal on scroll items as we want by reusing this class
 
 
 
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,14 +1,30 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-    constructor() {
-        this.itemsToReveal = $('.feature-item');
+    constructor(els, offset) {
+        this.itemsToReveal = els;
+        this.offsetPercentage = offset;
         this.hideInitially();
+        this.createWaypoints();
     }
     
-    hideInitially() {
-        this.itemsToReveal.addClass('reveal-item');
-        
+    hideInitially(){
+        this.itemsToReveal.addClass('reveal-item');   
+    }
+    
+    createWaypoints(){
+        var that = this;
+        this.itemsToReveal.each(function(){
+            var currentItem = this;
+            new Waypoint({
+                element: currentItem, //what we want to target    
+                handler: function(){  // what we want to happen
+                    $(currentItem).addClass('reveal-item--is-visible');
+                },
+                offset: that.offsetPercentage //customises waypoints when to trigger 
+            });
+        });
     }
 }
 
